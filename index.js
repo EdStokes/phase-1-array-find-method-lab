2983400accc5d5356e67df8e6f93cf6305d9be23
Ed Stokes <edstokes96@gmail.com>
2023-03-28 14:48:37 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+
+function superbowlWin(array){
+    let win = array.find(array => array.result === "W")
+    if (win){
+        return win.year;
+    }else return win;
+}
